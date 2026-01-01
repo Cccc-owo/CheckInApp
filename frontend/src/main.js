7d3d192416dfdcd105f6e8087ecf24e6d17eb8d2
Cccc_ <me@iscccc.eu.org>
2026-01-02 02:56:27 +0800

    fix: build

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import {
   User,
   Key,
